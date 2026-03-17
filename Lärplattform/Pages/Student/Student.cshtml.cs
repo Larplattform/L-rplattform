0e6e87d8fd7aef290b67987683ad4a69b7a09f9d
Mustafee <mussemoh03@gmail.com>
2026-03-17 17:59:28 +0100

    added teacher, admin and student folder

---
+++ Lärplattform/Pages/Student/Student.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lärplattform.Pages.Student
+{
+    public class StudentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
