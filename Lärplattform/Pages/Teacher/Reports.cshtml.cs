06af50a0a290bf0026df28f57f75648b328e1a21
Mustafee <mussemoh03@gmail.com>
2026-04-15 18:33:06 +0200

    finished teacher dashboard

---
+++ Lärplattform/Pages/Teacher/Reports.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lärplattform.Pages.Teacher
+{
+    public class ReportsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
