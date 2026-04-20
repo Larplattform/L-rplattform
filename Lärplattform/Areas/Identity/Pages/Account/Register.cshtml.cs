6c301f5e1b6495cc1d86a4e5b9c1c343c61e8b68
Mustafee <mussemoh03@gmail.com>
2026-04-20 20:48:24 +0200

    updated nuget

--- Lärplattform/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Lärplattform/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -5,21 +5,14 @@
 
 using Data.Entities;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
 using System.Text;
 using System.Text.Encodings.Web;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Lärplattform.Areas.Identity.Pages.Account
 {
