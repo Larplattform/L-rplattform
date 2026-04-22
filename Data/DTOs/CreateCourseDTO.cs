01a0ad610322d7ee87e830acc4e4bcf489564a12
Mustafee <mussemoh03@gmail.com>
2026-04-22 16:49:02 +0200

    added interfaces and services for courses

---
+++ Data/DTOs/CreateCourseDTO.cs
@@ -0,0 +1,18 @@
+﻿using Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.DTOs
+{
+    internal class CreateCourseDTO
+    {
+        public string SubjectName { get; set; } = string.Empty;
+        public int TotalMarks { get; set; }
+
+        public string ClassName { get; set; } = string.Empty;
+        public int TeacherID { get; set; }
+
+        public ICollection<CreateUserDTO> Users { get; set; } = new List<CreateUserDTO>();
+    }
+}
---
+++ Data/DTOs/CreateUserDTO.cs
@@ -0,0 +1,23 @@
+﻿using Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.DTOs
+{
+    internal class CreateUserDTO
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string Street { get; set; } = string.Empty;
+
+        public ICollection<CreateCourseDTO> Courses { get; set; } = new List<CreateCourseDTO>();
+
+
+
+        public bool IsDeleted { get; set; }
+    }
+}
