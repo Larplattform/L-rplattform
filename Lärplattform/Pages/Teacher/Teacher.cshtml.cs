9c6e3c98b7ad2d1cd9b06a587f5ff82179764672
Mustafee <mussemoh03@gmail.com>
2026-04-25 19:15:45 +0200

    fixed so only teacher can see their own courses

--- Lärplattform/Pages/Teacher/Teacher.cshtml.cs
+++ Lärplattform/Pages/Teacher/Teacher.cshtml.cs
@@ -1,5 +1,7 @@
 using Data.DTOs;
+using Data.Entities;
 using Lärplattform.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -8,23 +10,34 @@ namespace Lärplattform.Pages.Teacher
     public class TeacherModel : PageModel
     {
         public readonly IHttpClientFactory httpClientFactory;
+        public readonly UserManager<User> _userManager;
         public List<CourseViewModel> Courses { get; set; } = new List<CourseViewModel>();
 
-        public TeacherModel(IHttpClientFactory httpClientFactory)
+        public TeacherModel(IHttpClientFactory httpClientFactory, UserManager<User> userManager)
         {
             this.httpClientFactory = httpClientFactory;
+            _userManager = userManager;
         }
 
 
 
         public async Task<IActionResult> OnGetAsync()
         {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+            {
+                return RedirectToPage("/Account/Login");
+            }
+
+            var currentUserId = user.Id; 
+
             var client = httpClientFactory.CreateClient("APIClient");
             var response = await client.GetAsync("api/Course");
             if (response.IsSuccessStatusCode)
             {
                 var courseDTOs = await response.Content.ReadFromJsonAsync<List<CourseDTO>>() ?? new List<CourseDTO>();
-                Courses = courseDTOs.Select(dto => new CourseViewModel
+                Courses = courseDTOs.Where(dto => dto.TeacherID == currentUserId).Select(dto => new CourseViewModel
                 {
                     CourseID = dto.CourseID,
                     SubjectName = dto.SubjectName,
